87269f3d1964aba8fde80b2c0028a2ecd2bac162
Dai Akatsuka <d.akatsuka@gmail.com>
2017-08-30 02:17:09 +0900

    Add UnauthorizedException

--- src/main/scala/com/github/dakatsuka/akka/http/oauth2/client/Client.scala
+++ src/main/scala/com/github/dakatsuka/akka/http/oauth2/client/Client.scala
@@ -10,21 +10,24 @@ import com.github.dakatsuka.akka.http.oauth2.client.strategy.Strategy
 import scala.concurrent.{ ExecutionContext, Future }
 
 class Client(config: Config)(implicit system: ActorSystem) {
+  import Client._
+
   def getAuthorizeUrl[A <: GrantType](grant: A, params: Map[String, String] = Map.empty)(implicit s: Strategy[A]): Option[Uri] =
     s.getAuthorizeUrl(config, params)
 
-  def getAccessToken[A <: GrantType, OUT](grant: A, params: Map[String, String] = Map.empty)(
-      f: HttpResponse => Future[OUT]
-  )(implicit s: Strategy[A], ec: ExecutionContext, mat: Materializer): Future[Either[Throwable, OUT]] = {
+  def getAccessToken[A <: GrantType](
+      grant: A,
+      params: Map[String, String] = Map.empty
+  )(implicit s: Strategy[A], ec: ExecutionContext, mat: Materializer): Future[Either[Throwable, AccessToken]] = {
     val source = s.getAccessTokenSource(config, params)
 
     source
       .via(connection)
       .map { response =>
-        if (response.status.isFailure()) throw new Exception(response.toString())
+        if (response.status.isFailure()) throw new UnauthorizedException(response)
         response
       }
-      .mapAsync(1)(f)
+      .mapAsync(1)(AccessToken.apply)
       .runWith(Sink.head)
       .map(Right.apply)
       .recover {
@@ -36,4 +39,8 @@ class Client(config: Config)(implicit system: ActorSystem) {
     case "http"  => Http().outgoingConnection(config.site.getHost, config.site.getPort)
     case "https" => Http().outgoingConnectionHttps(config.site.getHost, config.site.getPort)
   }
+}
+
+object Client {
+  class UnauthorizedException(response: HttpResponse) extends RuntimeException(response.toString)
 }
